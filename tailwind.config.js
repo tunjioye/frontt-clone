4e448c998e5b94c6394cfd636304a2b86ad378f2
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2021-09-03 15:12:52 +0100

    SETUP : tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.sky,
+      red: colors.rose,
+      green: colors.emerald,
+      indigo: colors.indigo,
+    },
+    fontFamily: {
+      sans: ['system-ui', 'sans-serif'],
+      serif: ['Lora', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
